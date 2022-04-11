d9aa0ef6ea6e29290511e1408698b2929eb711a7
mundostr <idux.net@gmail.com>
2022-04-11 19:38:20 -0300

    control_claves funcionando

--- sem9/control_claves.py
+++ sem9/control_claves.py
@@ -2,19 +2,23 @@
 Objetivos:
 # 1) permitir que el usuario pueda ingresar una clave
 # 2) verificar ciertos criterios sobre ella utilizando una función
-3) mostrar un mensaje de error si la clave no cumple con los criterios
+# 3) mostrar mensaje según la clave cumpla o no los criterios
 
 Criterios:
 # 1) La clave debe tener al menos 8 caracteres de longitud.
-2) La clave debe tener al menos una letra mayúscula.
-3) La clave debe tener al menos una letra minúscula.
-4) La clave debe tener al menos un caracter especial.
-5) La clave debe tener al menos un dígito.
+# 2) La clave debe tener al menos una letra mayúscula.
+# 3) La clave debe tener al menos una letra minúscula.
+# 4) La clave debe tener al menos un dígito.
+# 5) La clave debe tener al menos un caracter especial.
 """
 
 
 # Constantes y variables
 CARACTERES_MINIMOS = 8
+CADENA_MAYUSCULAS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+CADENA_MINUSCULAS = "abcdefghijklmnopqrstuvwxyz"
+DIGITOS = "0123456789"
+CARACTERES_ESPECIALES = "@#$&_-"
 
 clave = "" # variable global
 clave_valida = False
@@ -23,15 +27,41 @@ clave_valida = False
 # Funciones
 def solicitar_clave():
     global clave
+    
+    print("La clave debe tener longitud mínima de 8 caracteres, mezclar mayúsculas, minúsculas, dígitos y caracteres especiales.")
+    print("Caracteres especiales válidos: @#$&_-")
+
     clave = input("Ingrese nueva clave: ")
 
 
 def verificar_clave():
-    global clave, clave_valida
+    global clave_valida
+    
+    longitud_ok = False
+    mayusculas_ok = False
+    minusculas_ok = False
+    digitos_ok = False
+    especiales_ok = False
     
     clave_valida = False
 
     if (len(clave) >= CARACTERES_MINIMOS):
+        longitud_ok = True
+    
+    for caracter in clave:
+        if (caracter in CADENA_MAYUSCULAS):
+            mayusculas_ok = True
+        
+        if (caracter in CADENA_MINUSCULAS):
+            minusculas_ok = True
+        
+        if (caracter in DIGITOS):
+            digitos_ok = True
+
+        if (caracter in CARACTERES_ESPECIALES):
+            especiales_ok = True
+
+    if (longitud_ok and mayusculas_ok and minusculas_ok and digitos_ok and especiales_ok):
         clave_valida = True
 
 
