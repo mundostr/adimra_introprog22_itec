2f3c060877bca10d822a6df1d987237f18fdfbb5
mundostr <idux.net@gmail.com>
2022-04-11 19:44:53 -0300

    control_claves con 5 criterios

--- sem9/control_claves.py
+++ sem9/control_claves.py
@@ -26,43 +26,43 @@ clave_valida = False
 
 # Funciones
 def solicitar_clave():
-    global clave
-    
-    print("La clave debe tener longitud mínima de 8 caracteres, mezclar mayúsculas, minúsculas, dígitos y caracteres especiales.")
-    print("Caracteres especiales válidos: @#$&_-")
+	global clave
+	
+	print("La clave debe tener longitud mínima de 8 caracteres, mezclar mayúsculas, minúsculas, dígitos y caracteres especiales.")
+	print("Caracteres especiales válidos: @#$&_-")
 
-    clave = input("Ingrese nueva clave: ")
+	clave = input("Ingrese nueva clave: ")
 
 
 def verificar_clave():
-    global clave_valida
-    
-    longitud_ok = False
-    mayusculas_ok = False
-    minusculas_ok = False
-    digitos_ok = False
-    especiales_ok = False
-    
-    clave_valida = False
+	global clave_valida
+	
+	longitud_ok = False
+	mayusculas_ok = False
+	minusculas_ok = False
+	digitos_ok = False
+	especiales_ok = False
+	
+	clave_valida = False
 
-    if (len(clave) >= CARACTERES_MINIMOS):
-        longitud_ok = True
-    
-    for caracter in clave:
-        if (caracter in CADENA_MAYUSCULAS):
-            mayusculas_ok = True
-        
-        if (caracter in CADENA_MINUSCULAS):
-            minusculas_ok = True
-        
-        if (caracter in DIGITOS):
-            digitos_ok = True
+	if (len(clave) >= CARACTERES_MINIMOS):
+		longitud_ok = True
+	
+	for caracter in clave:
+		if (caracter in CADENA_MAYUSCULAS):
+			mayusculas_ok = True
+		
+		if (caracter in CADENA_MINUSCULAS):
+			minusculas_ok = True
+		
+		if (caracter in DIGITOS):
+			digitos_ok = True
 
-        if (caracter in CARACTERES_ESPECIALES):
-            especiales_ok = True
+		if (caracter in CARACTERES_ESPECIALES):
+			especiales_ok = True
 
-    if (longitud_ok and mayusculas_ok and minusculas_ok and digitos_ok and especiales_ok):
-        clave_valida = True
+	if (longitud_ok and mayusculas_ok and minusculas_ok and digitos_ok and especiales_ok):
+		clave_valida = True
 
 
 # Procesos, flujo principal, salidas
@@ -71,6 +71,6 @@ solicitar_clave()
 verificar_clave()
 
 if (clave_valida == True):
-    print("La clave ingresada es válida")
+	print("La clave ingresada es válida")
 else:
-    print("La clave ingresada no es válida")
+	print("La clave ingresada no es válida")
